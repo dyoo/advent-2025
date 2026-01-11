2e129173235f1bd9e18abe863d152042b3d12df3
Danny Yoo <danny.yoo@gmail.com>
2026-01-11 13:15:42 -0800

    Continuing to play with good_lp

--- src/bin/lp.rs
+++ src/bin/lp.rs
@@ -1,21 +1,19 @@
-use good_lp::{Solution, SolverModel, default_solver, variable, variables};
+use good_lp::{Solution, SolverModel, variable, ProblemVariables};
+use good_lp::solvers::lpsolve;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    variables! {
-        vars:
-        a <= 1;
-        2 <= b <= 4;
-    }
+fn main() -> std::result::Result<(), Box<dyn Error>> {
+    let mut vars = ProblemVariables::new();
+    let a = vars.add(variable().max(9));
+    let b = vars.add(variable().min(2).max(4));
 
-    // ... or add variables programmatically
-    vars.add(variable().min(2).max(9));
-
-    let solution = vars
+    let problem = vars
         .maximise(10 * (a - b / 5) - b)
-        .using(default_solver)
-        .with(a + 2. << b) // or (a + 2).leq(b)
-        .with(1 + a >> 4. - b)
+        .using(lpsolve::lp_solve);
+  
+    let solution = problem
+        .with((a + 2).leq(b))
+        .with((1 + a).geq(4. - b))
         .solve()?;
 
     println!("a={:?}, b={:?}", solution.value(a), solution.value(b));
