da810f18b2338b3c7c73e9570a1d691bbdc7b706
Danny Yoo <danny.yoo@gmail.com>
2026-01-11 18:23:15 -0800

    day 11 part 1 complete.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -2,10 +2,7 @@ use std::collections::HashMap;
 use std::io::BufRead;
 use std::io::stdin;
 
-#[derive(Debug)]
-pub struct Graph {
-    pub edges: HashMap<String, Vec<String>>,
-}
+type Graph = HashMap<String, Vec<String>>;
 
 fn parse_graph(buf: impl BufRead) -> Graph {
     let edges: HashMap<String, Vec<String>> = buf
@@ -18,10 +15,53 @@ fn parse_graph(buf: impl BufRead) -> Graph {
             Some((name.to_string(), chunk.map(|str| str.to_string()).collect()))
         })
         .collect();
-    Graph { edges }
+    edges
+}
+
+fn count_paths_to_out(graph: &Graph, path: &[&String]) -> usize {
+    let at = path.last().copied().expect("empty path");
+
+    if at == "out" {
+        return 1;
+    }
+
+    let empty = vec![];
+    let children = graph
+        .get(at)
+        .unwrap_or(&empty)
+        .iter()
+        .filter(|child| !path.contains(child));
+
+    children
+        .map(|child| {
+            let mut new_path = path.to_vec();
+            new_path.push(child);
+            count_paths_to_out(graph, &new_path)
+        })
+        .sum()
+}
+
+#[test]
+fn test_count_paths() {
+    let input = "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"
+        .as_bytes();
+    let graph = parse_graph(input);
+    assert_eq!(count_paths_to_out(&graph, &vec![&"you".to_string()]), 5);
 }
 
 fn main() {
     let graph = parse_graph(stdin().lock());
-    println!("{:?}", graph);
+    println!(
+        "Part 1: {}",
+        count_paths_to_out(&graph, &vec![&"you".to_string()])
+    );
 }
